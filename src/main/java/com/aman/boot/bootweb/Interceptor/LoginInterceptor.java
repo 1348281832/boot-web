c3900c675450556d99ddbfed069cbc20060e6c74
Aman <1348281832qq.com>
2021-11-06 19:12:05 +0800

    third commit
    Signed-off-by: Aman <1348281832qq.com>

--- src/main/java/com/aman/boot/bootweb/Interceptor/LoginInterceptor.java
+++ src/main/java/com/aman/boot/bootweb/Interceptor/LoginInterceptor.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpSession;
 @Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
     /**
+     * hotfix
      * 执行之前
      * @param request
      * @param response
